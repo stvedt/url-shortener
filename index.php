e8e73c9392f91dc9b1ddf2fa547ef35e002d1bcd
Stephen Tvedt <svensat@gmail.com>
2012-08-30 14:52:52 -0400

    add validation of input for urls without http://

--- index.php
+++ index.php
@@ -26,6 +26,9 @@ $(document).ready(function() {
 });
 function getUrl(){
 	var url = $('#url').val();
+	if(!(new RegExp('http://')).test(url)){
+		url = 'http://' + url;
+	}
 	$.post('url.php', {url:url}, function(data){
 	
 	var message = $('#message p');
@@ -56,7 +59,7 @@ function getUrl(){
 <div id="container">
   <h1>Simple URL's @ tvdt.us</h1>
   <p>Go ahead, enter a long URL and have it shortened.</p>
-  <p><input type="text" name="url" id="url" size="60" autofocus placeholder="http://" /> <input id="button" type="button" value="Shorten" /></p>
+  <p><input type="text" name="url" id="url" size="60" autofocus placeholder="http://" required /> <input id="button" type="button" value="Shorten" /></p>
 
   <div id="message"><p>&nbsp;</p></div>
 
