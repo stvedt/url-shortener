190d04eaf77227623fd1e11ece2b8cfd1612f180
Stephen Tvedt <svensat@gmail.com>
2012-09-12 16:18:25 -0400

    added control + c / cmd + c tooltip

--- index.php
+++ index.php
@@ -12,19 +12,21 @@ if(isset($_GET['code']) && !empty($_GET['code'])){
 <head>
 <meta charset="UTF-8">
 <title>Tvdt.us - Clean, Short, URLs</title>
-
 <link href="style.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="//use.typekit.net/uzk6kjm.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 <script type="text/javascript">
+var opSys, tip;
 $(document).ready(function() {
     //Submit events
 	$('#button').click(getUrl);
 	$('#url').keypress(function(e) {
 		if(e.which == 13) {getUrl();}
 	});
+	
+	tip = $('#tip');
+	opSys = getOs();
 	
 });
 function getUrl(){
@@ -34,39 +36,48 @@ function getUrl(){
 	}
 	$.post('url.php', {url:url}, function(data){
 	
-	var message = $('#message p');
-	
-	switch (data)
-	{
-		case 'error_no_url':
-			message.html('No Url');
-			break;
-		case 'error_invalid_url':
-			message.html('Invalid Url');
-			break;
-		case 'error_is_min':
-				message.html('Already Shortened');
-			break;			
-		default:
-			$('#url').val(data);
-			$('#url').select();
-			message.html('Successfully Shortened URL');
-	}	
+		var message = $('#message p');
+		
+		switch (data)
+		{
+			case 'error_no_url':
+				message.html('No Url');
+				break;
+			case 'error_invalid_url':
+				message.html('Invalid Url');
+				break;
+			case 'error_is_min':
+					message.html('Already Shortened');
+				break;			
+			default:
+				$('#url').val(data);
+				$('#url').select();
+				tip.html(opSys);
+				tip.css('visibility','visible');
+				message.html('Successfully Shortened URL');
+		}	
 	});
 }
-</script>
 
+function getOs(){
+	var os =  navigator.platform;
+	return (os.match(/Mac/)) ? '⌘ + C to copy' : 'Ctrl + C to copy';
+}
+</script>
 </head>
 <body>
-
 <div id="container">
-  <h1><img src="tvdt.png" align="tvdt.us logo"/></h1>
-  <p>Enter an uncool link and make it hip!</p>
-  <p><input type="text" name="url" id="url" size="60" autofocus placeholder="http://" required /> <input id="button" type="button" value="Shorten" /></p>
-
-  <div id="message"><p>&nbsp;</p></div>
+	<h1><img src="tvdt.png" align="tvdt.us logo"/></h1>
+	<p>Enter an uncool link and make it hip!</p>
+	<p>
+		<input type="text" name="url" id="url" size="60" autofocus placeholder="http://" required />
+		<input id="button" type="button" value="Shorten" />
+	</p>
+	<div id="tip">Press Ctrl + C to copy.</div>
+	<div id="message">
+		<p>&nbsp;</p>
+	</div>
 	<p class="copyright">Brought to you by: <a href="http://www.stephentvedt.com">Stephen Tvedt</a></p>
 </div>
-
 </body>
 </html>
