3033efbbcba0cd7643619d6c617a073b4e12d38d
Stephen Tvedt <stvedt@kbsp.com>
2012-12-05 21:16:41 -0500

    move javascript from head tag to js file | update link for css and image to new folder for js/images paths

--- index.php
+++ index.php
@@ -16,72 +16,11 @@ if(isset($_GET['code']) && !empty($_GET['code'])){
 <script type="text/javascript" src="//use.typekit.net/uzk6kjm.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-<script type="text/javascript">
-var opSys, tip;
-$(document).ready(function() {
-    //Submit events
-	$('#button').click(getUrl);
-	var url = $('#url');
-	tip = $('#tip p');
-	opSys = getOs();
-	
-	url.keypress(function(e) {
-		if(e.which == 13) {getUrl();}
-	});
-	
-	url.blur( function(){
-		tip.fadeOut();	
-	});
-	url.focus( function(){
-		if (url.val().match(/tvdt.us/)){
-			tip.fadeIn();					
-			url.select();
-		}
-	}).mouseup(function(e){ e.preventDefault(); });
-	
-	
-	
-});
-function getUrl(){
-	var url = $('#url').val();
-	var urlBox = $('#url');
-	
-	if(!(new RegExp('^(http|https)://')).test(url)){
-		url = 'http://' + url;
-	}
-	$.post('url.php', {url:url}, function(data){
-	
-		var message = $('#message p');
-		
-		switch (data)
-		{
-			case 'error_no_url':
-				message.html('No Url');
-				break;
-			case 'error_invalid_url':
-				message.html('Invalid Url');
-				break;
-			case 'error_is_min':
-					message.html('Already Shortened');
-				break;			
-			default:
-				urlBox.val(data);
-				message.html('Successfully Shortened URL');
-		}
-		tip.html(opSys);
-		urlBox.select();
-	});
-}
-
-function getOs(){
-	var os =  navigator.platform;
-	return (os.match(/Mac/)) ? '⌘ + C to copy' : 'Ctrl + C to copy';
-}
-</script>
+<script src="js/url.js"></script>
 </head>
 <body>
 <div id="container">
-	<h1><img src="tvdt.png" align="tvdt.us logo"/></h1>
+	<h1><img src="tvdt.png" align="images/tvdt.us logo"/></h1>
 	<p>Enter an uncool link and make it hip!</p>
 	<p>
 		<input type="url" name="url" id="url" size="60" autofocus placeholder="http://" required />
